506c4cba0966bb0d3008c8c2103bf8e59fbc499f
pbrodriguezm <pb.rodriguez.m@gmail.com>
2024-04-10 11:00:03 -0500

    No result

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 'use strict'
 const GI = {
-	createElement (tag, atributes) {
-		let element = document
-		return document.createElement(tag)
-	}
+  createElement(tag, atributes) {
+    let element = document
+    console.log('==>>');
+    return document.createElement(tag)
+  }
 }
 
 
+
+console.log(elements.atributes);
 const to_do = [
-    'Alimentar al perro',
-    'Estudiar para prueba escrita',
-    'Pagar taxes',
-    'Pasear a perro',
-    'Comprar entradas para el cine',
+  'Alimentar al perro',
+  'Estudiar para prueba escrita',
+  'Pagar taxes',
+  'Pasear a perro',
+  'Comprar entradas para el cine',
 ]
 
+const elements = document.getElementsByClassName("js-cont-todo");
+to_do.forEach(data => {
+  elements.item('').append(data);
+})
